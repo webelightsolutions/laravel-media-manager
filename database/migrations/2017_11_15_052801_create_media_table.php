daf4aa3736168499e3a0ed4b720e4499dd7fd36b
vivek <vivek.kachhadiya23@gmail.com>
2017-11-21 17:00:21 +0530

    Add morph relation in model and in database.

--- database/migrations/2017_11_15_052801_create_media_table.php
+++ database/migrations/2017_11_15_052801_create_media_table.php
@@ -20,10 +20,11 @@ class CreateMediaTable extends Migration
             $table->timestamps();
             $table->datetime('deleted_at');
             $table->boolean('is_active')->default(0); //0:Active 1:Deactive
-            $table->string('media_name')->comment('Media Name.');
+            $table->integer('model_id')->unsigned()->nullable()->index()->comment('Model id');
+            $table->string('model_type')->nullable()->index()->comment('Model Name');
+            $table->string('media_name')->comment('Media Name');
             $table->string('mime_type')->comment('Media Type')->nullable();
             $table->longText('path');
-            $table->longText('thumbnail_path');
         });
     }
 
--- src/Media.php
+++ src/Media.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Media extends Model
 {
     protected $table = 'media';
-    protected $fillable = ['media_name', 'mime_type', 'path', 'thumbnail_path'];
+    protected $fillable = ['model_id', 'model_type', 'media_name', 'mime_type', 'path', 'thumbnail_path'];
+
+    public function media()
+    {
+    	return $this->morphTo();
+    }
 }
